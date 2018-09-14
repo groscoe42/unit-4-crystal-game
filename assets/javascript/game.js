9fd3d25601b636ff8dd4bf850afbc48579238511
GreenJeans <40717979+groscoe42@users.noreply.github.com>
2018-09-14 13:41:29 -0400

    add files

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+// declare variables
+var win = 0;
+var lose = 0;
+var randNum = [Math.floor(Math.random(19) * 120)];
+var cPick = randNum
+var c1 = Math.floor(Math.random() * 12)+1;
+var c2 = Math.floor(Math.random() * 12)+1;
+var c3 = Math.floor(Math.random() * 12)+1;
+var c4 = Math.floor(Math.random() * 12)+1;
+var numlist = [c1,c2,c3,c4];
+var uNum = 0;
+
+
+$("#target").text(" hello ");
+console.log(randNum);
+
+// create reset function
+
+//click events
+
+// win conditions
+
+});
